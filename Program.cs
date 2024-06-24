8bbe95911703869a6d8c6dc24714bc04c9bda5c2
Rampage Maslenka <92434423+Maslenka-dev@users.noreply.github.com>
2024-06-24 19:57:00 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,75 +1,67 @@
-using System;
-using System.IO;
-
-namespace SteamKeys
-{
+ namespace SteamKeys
+ {
     internal class Program
     {
+        private static Random random = new Random();
+        private static void Main(string[] args)
+        {
+            Console.Title = "SteamKeys++";
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            const int keyLength = 15; // Фиксированная длина ключа
+            string fileName = "keys.txt"; // Путь текстовому файлу с ключами
 
-        private static int InputQuantidade()
+            KeyGenerator(chars, keyLength, InputQuantidade(), fileName);
+
+            EscreverOutput(fileName);
+        }
+        private static int InputQuantidade() // Изменяет интерфейс консоли и считывает количество ключей, которые вы хотите получить
         {
             string title = @"
 
-░██████╗████████╗███████╗░█████╗░███╗░░░███╗██╗░░██╗███████╗██╗░░░██╗░██████╗░░░░░░░░░░░░░░
-██╔════╝╚══██╔══╝██╔════╝██╔══██╗████╗░████║██║░██╔╝██╔════╝╚██╗░██╔╝██╔════╝░░██╗░░░░██╗░░
-╚█████╗░░░░██║░░░█████╗░░███████║██╔████╔██║█████═╝░█████╗░░░╚████╔╝░╚█████╗░██████╗██████╗
-░╚═══██╗░░░██║░░░██╔══╝░░██╔══██║██║╚██╔╝██║██╔═██╗░██╔══╝░░░░╚██╔╝░░░╚═══██╗╚═██╔═╝╚═██╔═╝
-██████╔╝░░░██║░░░███████╗██║░░██║██║░╚═╝░██║██║░╚██╗███████╗░░░██║░░░██████╔╝░░╚═╝░░░░╚═╝░░
-╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═╝░░╚═╝╚══════╝░░░╚═╝░░░╚═════╝░░░░░░░░░░░░░░░
+            ░██████╗████████╗███████╗░█████╗░███╗░░░███╗██╗░░██╗███████╗██╗░░░██╗░██████╗░░░░░░░░░░░░░░
+            ██╔════╝╚══██╔══╝██╔════╝██╔══██╗████╗░████║██║░██╔╝██╔════╝╚██╗░██╔╝██╔════╝░░██╗░░░░██╗░░
+            ╚█████╗░░░░██║░░░█████╗░░███████║██╔████╔██║█████═╝░█████╗░░░╚████╔╝░╚█████╗░██████╗██████╗
+            ░╚═══██╗░░░██║░░░██╔══╝░░██╔══██║██║╚██╔╝██║██╔═██╗░██╔══╝░░░░╚██╔╝░░░╚═══██╗╚═██╔═╝╚═██╔═╝
+            ██████╔╝░░░██║░░░███████╗██║░░██║██║░╚═╝░██║██║░╚██╗███████╗░░░██║░░░██████╔╝░░╚═╝░░░░╚═╝░░
+            ╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═╝░░╚═╝╚══════╝░░░╚═╝░░░╚═════╝░░░░░░░░░░░░░░░
                                     
                                     
-";
+            ";
+
             Console.WriteLine(title, Console.ForegroundColor = ConsoleColor.Cyan);
             Console.WriteLine();
-            Console.ResetColor();
-            Console.Write("Insert how many keys you want to generate: ");
-            int qtdKeys = int.Parse(Console.ReadLine());
+            Console.Write("Insert how many keys you want to generate: ", Console.ForegroundColor = ConsoleColor.White);
+            int numberOfKeys = Convert.ToInt32(Console.ReadLine()); // Считывание числа 
             Console.WriteLine();
-            return qtdKeys;
+            return numberOfKeys;
         }
-        private static void GeraSalvaKeys(Random random, string chars, int keyLength, int qtdKeys, string fileName)
+        private static void KeyGenerator(string chars, int keyLength, int numberOfKeys, string fileName) // Генерирует ключи
         {
-            using (StreamWriter writer = new StreamWriter(fileName))
+            StreamWriter writer = new StreamWriter(fileName, false); // Объект класса StreamWriter. Реализует запись ключей в текстовый файл
+            for (int i = 0; i < numberOfKeys; i++)
             {
-                for (int i = 0; i < qtdKeys; i++)
+                char[] charsOfKey = new char[keyLength]; // Массив сгенерированных символов
+                for (int j = 0; j < keyLength; j++)
                 {
-                    char[] keyChars = new char[keyLength];
-
-                    for (int j = 0; j < keyLength; j++)
-                    {
-                        keyChars[j] = chars[random.Next(chars.Length)];
-                    }
-
-                    string key = new string(keyChars);
-                    string formattedKey = string.Format("{0}-{1}-{2}", key.Substring(0, 5), key.Substring(5, 5), key.Substring(10, 5));
-                    Console.WriteLine(formattedKey, Console.ForegroundColor = ConsoleColor.Magenta);
-                    writer.WriteLine(formattedKey);
+                    charsOfKey[j] = chars[random.Next(chars.Length)];
                 }
+
+                string key = new string(charsOfKey);
+                string formattedKey = $"{key.Substring(0, 5)}-{key.Substring(5, 5)}-{key.Substring(10, 5)}"; // Ключ в нормальном виде
+                Console.WriteLine(formattedKey, Console.ForegroundColor = ConsoleColor.Magenta);
+                writer.WriteLine(formattedKey);
             }
+            writer.Close();
         }
-        private static void EscreverOutput(string fileName)
+        private static void EscreverOutput(string fileName) // Изменяет интерфейс консоли и отображает в какой файл сохранились ключи
         {
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Keys saved to file: " + fileName);
+            Console.WriteLine("Keys saved to file: " + fileName); 
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Press ENTER to exit");
             Console.ResetColor();
             Console.ReadKey();
         }
-        static void Main(string[] args)
-        {
-            Console.Title = "SteamKeys++"; // Название окна потому что я тупой
-            Random random = new Random();
-            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            int keyLength = 15;
-            string fileName = "keys.txt";  
-            int qtdKeys = InputQuantidade();
-
-            GeraSalvaKeys(random, chars, keyLength, qtdKeys, fileName);
-
-            EscreverOutput(fileName);
-        }
-
     }
 }
